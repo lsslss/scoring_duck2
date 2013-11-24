1c24b19cf6e06eef42b958425112da1098d65a74
lsslss <lsslss.git@gmail.com>
2013-11-24 23:48:17 +0800

    標題內容換行

--- scoring_duck2.user.js
+++ scoring_duck2.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name       scoring duck2
 // @namespace  http://web.nlhs.tyc.edu.tw/~lss
-// @version    0.4.1
+// @version    0.4.2
 // @description  scoring duck2.htm homework
 // @match      http://web.nlhs.tyc.edu.tw/*/duck2.htm
 // @downloadURL     https://raw.github.com/lsslss/scoring_duck2/master/scoring_duck2.user.js
 // @updateURL       https://raw.github.com/lsslss/scoring_duck2/master/scoring_duck2.user.js
-// @updateVersion   14
+// @updateVersion   15
 // @copyright  2012+, You
 // ==/UserScript==
 var duckTitle = document.getElementsByTagName('h1')[0];
@@ -23,12 +23,12 @@ scoringTitle.style.margin = '10';
 scoringP.style.margin = '10';
 scoringNLA.style.margin = '10';
 scoringWiki.style.margin = '10';
-scoringTitle.textContent = '網頁標題：[' + document.getElementsByTagName('title')[0].textContent + ']';
+scoringTitle.textContent = '網頁標題：<br />[' + document.getElementsByTagName('title')[0].textContent + ']';
 var allPs = document.getElementsByTagName('p');
 scoringP.textContent = '==================>沒有新增段落';
 if(allPs.length >= 2)
 {
-    scoringP.textContent = '新增段落：[' + allPs[0].textContent + ']';
+    scoringP.textContent = '新增段落：<br />[' + allPs[0].textContent + ']';
 }
 var allLinks = document.getElementsByTagName('a');
 scoringNLA.textContent = '==================>沒有內壢高中超連結';
@@ -37,7 +37,7 @@ for(var i=0; i<allLinks.length; ++i)
     var aLink = allLinks[0];
     if(aLink.textContent == '內壢高中')
     {
-        scoringNLA.textContent = '內高網址：[' + aLink.href + ']';
+        scoringNLA.textContent = '內高網址：<br />[' + aLink.href + ']';
         break;
     }
 }
@@ -45,7 +45,7 @@ var allImgs = document.getElementsByTagName('img');
 scoringWiki.textContent = '==================>沒有唐老鴨超連結';
 if(allImgs[0].parentNode.tagName == 'A')
 {
-    scoringWiki.textContent = '唐老鴨網址：[' + allImgs[0].parentNode.href + ']';
+    scoringWiki.textContent = '唐老鴨網址：<br />[' + allImgs[0].parentNode.href + ']';
 }
 duckTitle.parentNode.insertBefore(homework, duckTitle);
 homework.appendChild(scoringTitle);
